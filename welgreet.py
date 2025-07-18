77a25e93082165743a3844d0b38ca71e0b5b1b4f
kajalgupta-08 <kajalgupta7997@gmail.com>
2025-07-18 08:15:22 +0530

    Create welgreet.py

---
+++ welgreet.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title="Greeting App", layout="centered")
+
+st.title("Welcome!")
+name = st.text_input("Enter your name:")
+
+if name:
+    st.markdown(f"Hi user{name}!")
